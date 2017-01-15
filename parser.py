5086658c777542faa32859fd020577c12af94089
CezaryGapinski <gapa1986@gmail.com>
2017-01-15 12:56:56 +0100

    Small refactoring

--- parser.py
+++ parser.py
@@ -12,6 +12,41 @@ class spiPins:
         self.mosi = mosi
         self.cs = cs
 
+def generateJinja2File(filename, templateFile, templateVars):
+    template = templateEnv.get_template( templateFile )
+    outputText = template.render( templateVars )
+    
+    file=open(filename, 'w')
+    file.write(outputText)
+    file.close()
+
+def generateSpiPinsListConfig(data):
+    spilist = [ spiPins(0, 0, 0, 0, 0) for i in data["spis_pins"]]     
+          
+    i = 0
+    for x in data["spis_pins"]:
+        for y in data["alternative_pins"]:
+            if x["clk"] == y["id"]:
+                spilist[i].clk = y
+                
+        for y in data["alternative_pins"]:
+            if x["miso"] == y["id"]:
+                spilist[i].miso = y
+                
+        for y in data["alternative_pins"]:
+            if x["mosi"] == y["id"]:
+                spilist[i].mosi = y
+                
+        for y in data["output_pins"]:
+            if x["cs"] == y["id"]:
+                spilist[i].cs = y
+                
+        spilist[i].id = x["id"]
+                
+        i += 1
+        
+    return spilist
+
 if (len(sys.argv) != 2):
     print "You must set argument!!!"
     sys.exit(2)
@@ -24,13 +59,13 @@ with open('pin_config.json') as data_file:
 templateLoader = jinja2.FileSystemLoader( searchpath="." )
 templateEnv = jinja2.Environment( loader=templateLoader )
 
-LEDS_HPP_TEMPLATE = "leds_hpp.jinja"
-LEDS_CPP_TEMPLATE = "leds_cpp.jinja"
+LEDS_HPP_TEMPLATE = "templates/leds_hpp.jinja"
+LEDS_CPP_TEMPLATE = "templates/leds_cpp.jinja"
 
-SPIS_HPP_TEMPLATE = "spi_hpp.jinja"
-SPIS_CPP_TEMPLATE = "spi_cpp.jinja"
+SPIS_HPP_TEMPLATE = "templates/spi_hpp.jinja"
+SPIS_CPP_TEMPLATE = "templates/spi_cpp.jinja"
 
-LOW_LEVEL_PIN_INIT_CPP_TEMPLATE = "lowLevelPinInitialization_cpp.jinja"
+LOW_LEVEL_PIN_INIT_CPP_TEMPLATE = "templates/lowLevelPinInitialization_cpp.jinja"
 
 leds_ids = []
 leds_id_to_out_pin_ids = []
@@ -47,30 +82,7 @@ for x in leds_id_to_out_pin_ids:
     if y["id"] == x:
       leds_pins.append(y)
      
-spilist = [ spiPins(0, 0, 0, 0, 0) for i in data["spis_pins"]]     
-      
-i = 0
-for x in data["spis_pins"]:
-    for y in data["alternative_pins"]:
-        if x["clk"] == y["id"]:
-            spilist[i].clk = y
-            
-    for y in data["alternative_pins"]:
-        if x["miso"] == y["id"]:
-            spilist[i].miso = y
-            
-    for y in data["alternative_pins"]:
-        if x["mosi"] == y["id"]:
-            spilist[i].mosi = y
-            
-    for y in data["output_pins"]:
-        if x["cs"] == y["id"]:
-            spilist[i].cs = y
-            
-    spilist[i].id = x["id"]
-            
-    i += 1
-      
+spilist = generateSpiPinsListConfig(data) 
       
 pins_type = set()
 for x in data["output_pins"]:
@@ -81,9 +93,6 @@ for x in data["alternative_pins"]:
     pin_without_number = str(x["pin"])
     pins_type.add(pin_without_number.translate(None, digits))
 
-for x in pins_type:   
-    print(x)
-      
 templateVars = {    "board" : data["board"],
                     "gpio_version" : data["gpio_driver_version"],
 		            "leds_number" : len(data["leds"]),
@@ -98,44 +107,18 @@ include_directory = cmdargs_path + "include/distortos/board"
 
 if not os.path.exists(include_directory):
     os.makedirs(include_directory)
-
-template = templateEnv.get_template( LEDS_HPP_TEMPLATE )
-outputText = template.render( templateVars )
-
+    
 filename = include_directory + "/" + "%s.hpp" % "leds"
-file=open(filename, 'w')
-file.write(outputText)
-file.close()
-
-template = templateEnv.get_template( LEDS_CPP_TEMPLATE )
-outputText = template.render( templateVars )
+generateJinja2File(filename, LEDS_HPP_TEMPLATE, templateVars)
 
 filename = cmdargs_path + "%s.cpp" % (data["board"] + "-leds")
-file=open(filename, 'w')
-file.write(outputText)
-file.close()
-
-template = templateEnv.get_template( SPIS_HPP_TEMPLATE )
-outputText = template.render( templateVars )
+generateJinja2File(filename, LEDS_CPP_TEMPLATE , templateVars)
 
 filename = include_directory + "/" + "%s.hpp" % "spi"
-file=open(filename, 'w')
-file.write(outputText)
-file.close()
-
-template = templateEnv.get_template( SPIS_CPP_TEMPLATE )
-outputText = template.render( templateVars )
+generateJinja2File(filename, SPIS_HPP_TEMPLATE , templateVars)
 
 filename = cmdargs_path + "%s.cpp" % (data["board"] + "-spi")
-file=open(filename, 'w')
-file.write(outputText)
-file.close()
-
-template = templateEnv.get_template( LOW_LEVEL_PIN_INIT_CPP_TEMPLATE )
-outputText = template.render( templateVars )
+generateJinja2File(filename, SPIS_CPP_TEMPLATE , templateVars)
 
 filename = cmdargs_path + "%s.cpp" % (data["board"] + "-lowLevelPinInitialization")
-file=open(filename, 'w')
-file.write(outputText)
-file.close()
-
+generateJinja2File(filename, LOW_LEVEL_PIN_INIT_CPP_TEMPLATE , templateVars)
