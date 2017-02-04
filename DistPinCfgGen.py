06dac67ad4e04c4288b2242262445dd60a6f27da
CezaryGapinski <gapa1986@gmail.com>
2017-02-04 18:05:10 +0100

    Adjusting to leds and buttons
    Script is for now used only to generate leds and buttons configuration.
    Adjusting to separated paths to GPIO peripherial types

--- DistPinCfgGen.py
+++ DistPinCfgGen.py
@@ -9,20 +9,15 @@
 #
 
 import json
-from jsonschema import validate
 import jinja2
 import os.path
 import sys
 import getopt
 from string import digits
 
-class spiPins:
-    def __init__(self, id, clk, miso, mosi, cs):
-        self.id = id
-        self.clk = clk
-        self.miso = miso
-        self.mosi = mosi
-        self.cs = cs
+gpio_perihperals_paths = {"v1":"jsonTemplatesGPIOv1",
+                          "v2":"jsonTemplatesGPIOv2"
+                          }
         
 def checkInputParams(argv, parameters):
     try:
@@ -50,33 +45,6 @@ def generateJinja2File(filename, templateFile, templateVars):
     file.write(outputText)
     file.close()
     
-def generateSpiPinsListConfig(data):
-    spilist = [ spiPins(0, 0, 0, 0, 0) for i in data["spis_pins"]]     
-          
-    i = 0
-    for x in data["spis_pins"]:
-        for y in data["alternative_pins"]:
-            if x["clk"] == y["id"]:
-                spilist[i].clk = y
-                
-        for y in data["alternative_pins"]:
-            if x["miso"] == y["id"]:
-                spilist[i].miso = y
-                
-        for y in data["alternative_pins"]:
-            if x["mosi"] == y["id"]:
-                spilist[i].mosi = y
-                
-        for y in data["output_pins"]:
-            if x["cs"] == y["id"]:
-                spilist[i].cs = y
-                
-        spilist[i].id = x["id"]
-                
-        i += 1
-        
-    return spilist
-
 parameters = ['', '']
 
 checkInputParams(sys.argv[1:], parameters)
@@ -84,30 +52,22 @@ checkInputParams(sys.argv[1:], parameters)
 with open(parameters[0]) as data_file:    
     data = json.load(data_file)
     
-with open("templates/pin_config_v2_schema.json") as data_file:    
-    schema = json.load(data_file)
-    
-print(schema)
-
-validate(data, schema)
-
 cmdargs_path = parameters[1]
 cmdargs_path += data["board"] + "/"
 
 templateLoader = jinja2.FileSystemLoader( searchpath="." )
 templateEnv = jinja2.Environment( loader=templateLoader )
 
-LEDS_HPP_TEMPLATE = "templates/leds_hpp.jinja"
-LEDS_CPP_TEMPLATE = "templates/leds_cpp.jinja"
+gpioTemplateDir = gpio_perihperals_paths[data["gpio_driver_version"]]
 
-BUTTONS_HPP_TEMPLATE = "templates/buttons_hpp.jinja"
-BUTTONS_CPP_TEMPLATE = "templates/buttons_cpp.jinja"
+LEDS_HPP_TEMPLATE = gpioTemplateDir + "/leds_hpp.jinja"
+LEDS_CPP_TEMPLATE = gpioTemplateDir + "/leds_cpp.jinja"
 
-SPIS_HPP_TEMPLATE = "templates/spi_hpp.jinja"
-SPIS_CPP_TEMPLATE = "templates/spi_cpp.jinja"
+BUTTONS_HPP_TEMPLATE = gpioTemplateDir + "/buttons_hpp.jinja"
+BUTTONS_CPP_TEMPLATE = gpioTemplateDir + "/buttons_cpp.jinja"
 
-LOW_LEVEL_PIN_INIT_HPP_TEMPLATE = "templates/lowLevelPinInitialization_hpp.jinja"
-LOW_LEVEL_PIN_INIT_CPP_TEMPLATE = "templates/lowLevelPinInitialization_cpp.jinja"
+LOW_LEVEL_PIN_INIT_HPP_TEMPLATE = gpioTemplateDir + "/lowLevelPinInitialization_hpp.jinja"
+LOW_LEVEL_PIN_INIT_CPP_TEMPLATE = gpioTemplateDir + "/lowLevelPinInitialization_cpp.jinja"
 
 leds_ids = []
 leds_id_to_out_pin_ids = []
@@ -139,14 +99,8 @@ for x in buttons_id_to_in_pin_ids:
     if y["id"] == x:
       buttons_pins.append(y)
      
-spilist = generateSpiPinsListConfig(data) 
-      
 pins_type = set()
 for x in data["output_pins"]:
-    pin_without_number = str(x["pin"])
-    pins_type.add(pin_without_number.translate(None, digits))
-    
-for x in data["alternative_pins"]:
     pin_without_number = str(x["pin"])
     pins_type.add(pin_without_number.translate(None, digits))
     
@@ -157,6 +111,7 @@ for x in data["input_pins"]:
 fileTypeInHeader = "EMPTY"
 
 templateVars = {    "board" : data["board"],
+                    "template_dir" : gpioTemplateDir,
                     "file_type_in_header" : fileTypeInHeader,
                     "gpio_version" : data["gpio_driver_version"],
 		            "leds_number" : len(data["leds"]),
@@ -165,8 +120,6 @@ templateVars = {    "board" : data["board"],
                     "buttons_number" : len(data["buttons"]),
                     "buttons_ids" : buttons_ids,
                     "buttons_pins" : buttons_pins,
-                    "spis_number" : len(data["spis_pins"]),
-                    "spi_pins_list" : spilist,
                     "used_pins_groups" : pins_type
                }
 
@@ -188,13 +141,6 @@ generateJinja2File(filename, BUTTONS_HPP_TEMPLATE, templateVars)
 
 filename = cmdargs_path + "%s.cpp" % (data["board"] + "-buttons")
 generateJinja2File(filename, BUTTONS_CPP_TEMPLATE , templateVars)
-
-templateVars["file_type_in_header"] = "SPI" 
-filename = include_directory + "/" + "%s.hpp" % "spi"
-generateJinja2File(filename, SPIS_HPP_TEMPLATE , templateVars)
-
-filename = cmdargs_path + "%s.cpp" % (data["board"] + "-spi")
-generateJinja2File(filename, SPIS_CPP_TEMPLATE , templateVars)
 
 templateVars["file_type_in_header"] = "lowLevelPinInitialization" 
 filename = include_directory + "/" + "%s.hpp" % "lowLevelPinInitialization"
