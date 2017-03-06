ddbf2b9a4e94db6c643b54f18d29c38ce368b322
CezaryGapinski <gapa1986@gmail.com>
2017-03-06 19:07:24 +0100

    Nex custom board

---
+++ examples/CUSTOM-BOARD/CUSTOM-BOARD-leds.cpp
@@ -0,0 +1,45 @@
+/**
+ * File generated with scripts/generateBoard.py:
+ * Copyright (C) 2017 Cezary Gapinski cezary.gapinski@gmail.com
+ *
+ * \file
+ * \brief Definition of LEDs for CUSTOM-BOARD
+ *
+ * \author Copyright (C) 2016-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "distortos/board/leds.hpp"
+
+#ifdef CONFIG_BOARD_LEDS_ENABLE
+
+#include "distortos/chip/ChipOutputPin.hpp"
+
+namespace distortos
+{
+
+namespace board
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+chip::ChipOutputPin leds[totalLeds]
+{
+#if DISTORTOS_BOARD_LD2_LED_ENABLE == 1
+		chip::ChipOutputPin{chip::Pin::pa5, false, chip::PinOutputSpeed::_2Mhz, false, false},
+#endif	// DISTORTOS_BOARD_LD2_LED_ENABLE == 1
+#if DISTORTOS_BOARD_LD3_LED_ENABLE == 1
+		chip::ChipOutputPin{chip::Pin::pg5, false, chip::PinOutputSpeed::_2Mhz, false, false},
+#endif	// DISTORTOS_BOARD_LD3_LED_ENABLE == 1
+};
+
+}	// namespace board
+
+}	// namespace distortos
+
+#endif	// def CONFIG_BOARD_LEDS_ENABLE
