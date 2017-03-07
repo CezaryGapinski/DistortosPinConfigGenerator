fc9995d2a361b96d93daf2975b1448eab05822bc
CezaryGapinski <gapa1986@gmail.com>
2017-03-07 20:50:15 +0100

    Generate files for NUCLEO-F429ZI

---
+++ examples/NUCLEO-F429ZI/NUCLEO-F429ZI-buttons.cpp
@@ -0,0 +1,42 @@
+/**
+ * File generated with scripts/generateBoard.py:
+ * Copyright (C) 2017 Cezary Gapinski cezary.gapinski@gmail.com
+ *
+ * \file
+ * \brief Definition of buttons for NUCLEO-F429ZI
+ *
+ * \author Copyright (C) 2016-2017 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "distortos/board/buttons.hpp"
+
+#ifdef CONFIG_BOARD_BUTTONS_ENABLE
+
+#include "distortos/chip/ChipInputPin.hpp"
+
+namespace distortos
+{
+
+namespace board
+{
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global objects
++---------------------------------------------------------------------------------------------------------------------*/
+
+const chip::ChipInputPin buttons[totalButtons]
+{
+#if DISTORTOS_BOARD_B1_BUTTON_ENABLE == 1
+		chip::ChipInputPin{chip::Pin::pc13, chip::PinPull::none, false},
+#endif	// DISTORTOS_BOARD_B1_BUTTON_ENABLE == 1
+};
+
+}	// namespace board
+
+}	// namespace distortos
+
+#endif	// def CONFIG_BOARD_BUTTONS_ENABLE
